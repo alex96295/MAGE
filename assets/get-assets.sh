5548de9306e19e245bcff08687309f119b0b0f3d
aottaviano <aottaviano@iis.ee.ethz.ch>
2025-09-10 17:43:19 +0200

    assets: Add assets directory and initial content

---
+++ assets/get-assets.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# Output folder
+OUTDIR="language"
+mkdir -p "$OUTDIR"
+
+# URLs (direct HTTP links, not chrome-extension)
+SV_URL="https://rfsoc.mit.edu/6S965/_static/F24/documentation/1800-2017.pdf"
+GOTCHAS_URL="https://picture.iczhiku.com/resource/eetop/wyKErzSAliSTINNx.pdf"
+
+# Filenames
+SV_OUT="$OUTDIR/SystemVerilog-1800-2017.pdf"
+GOTCHAS_OUT="$OUTDIR/101-SystemVerilog-Gotchas.pdf"
+
+echo "Downloading IEEE 1800-2017 SystemVerilog manual..."
+wget -O "$SV_OUT" "$SV_URL"
+
+echo "Downloading 101 SystemVerilog Gotchas book..."
+wget -O "$GOTCHAS_OUT" "$GOTCHAS_URL"
+
+echo "All files downloaded to: $OUTDIR"
+ls -lh "$OUTDIR"
